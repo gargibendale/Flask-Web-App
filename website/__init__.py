327ced597ae1a71acd036be83be66799fdea7c38
gargibendale <gargibendale@gmail.com>
2024-03-31 23:25:29 +0530

    update add flash messages

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET KEY'] = 'qwdhfgjklasrytuop'
+    app.config.update(SECRET_KEY=os.urandom(24))
 
     from .views import views
     from .auth import auth

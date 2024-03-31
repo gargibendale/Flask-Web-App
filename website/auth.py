52196d4aa2f7570a98b58ad4edc7c949cb382c40
gargibendale <gargibendale@gmail.com>
2024-03-31 22:30:40 +0530

    update add forms and flash messages

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods = ['GET', 'POST'])
 def login():
+    
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods = ['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        fname = request.form.get('fname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 chars.', category='error')
+        elif len(fname) < 2:
+            flash('First name must be greater than 1 chars.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash("Password must be atleast 7 chars.", category='error')
+        else:
+            #add user to the database
+            flash('Account created!', category='success')
     return render_template("signup.html")

e333c42cc956d77589c2f8ee6d997ed0da5dce42
gargibendale <gargibendale@gmail.com>
2024-04-01 12:24:55 +0530

    update set up account creation and login

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,23 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db #means from __init__.py import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Login successful', category='success')
+            else:
+                flash('Incorrect password X_X', category='error')
+        else:
+            flash('Email does not exist', category='error')
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -19,7 +32,11 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('User already exists', category='error')
+
+        elif len(email) < 4:
             flash('Email must be greater than 4 chars.', category='error')
         elif len(fname) < 2:
             flash('First name must be greater than 1 chars.', category='error')
@@ -29,5 +46,9 @@ def signup():
             flash("Password must be atleast 7 chars.", category='error')
         else:
             #add user to the database
+            new_user = User(email=email, first_name=fname, password=generate_password_hash(password1, method='scrypt'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
     return render_template("signup.html")

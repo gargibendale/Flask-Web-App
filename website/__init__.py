ea5faf45d5650ba27fb24fd0a640188c65cb21b2
gargibendale <gargibendale@gmail.com>
2024-04-01 11:01:19 +0530

    updates set up database

--- website/__init__.py
+++ website/__init__.py
@@ -9,6 +9,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config.update(SECRET_KEY=os.urandom(24))
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
